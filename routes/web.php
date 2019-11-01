acdc8bf429081db7be28b3a366878df5a6118332
isaacoduh <ufuomaoduh@gmail.com>
2019-11-01 23:38:05 +0100

    tags CRUD

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('categories', 'CategoriesController');
     Route::resource('posts', 'PostsController');
+    Route::resource('tags', 'TagsController');
     Route::get('trashed-posts', 'PostsController@trashed')->name('trashed-posts.index');
     Route::put('restore-post/{post}', 'PostsController@store')->name('restore-posts');    
 });
