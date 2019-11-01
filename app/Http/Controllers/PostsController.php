dd58feda0c5c14b07417b4a79aab6bdecb858c4c
isaacoduh <ufuomaoduh@gmail.com>
2019-11-01 22:07:47 +0100

    HasMany & BelongsTo; Category & Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Post\CreatePostsRequest;
 use App\Post;
 use Illuminate\Http\Request;
+use App\Category;
 use App\Http\Requests\Post\UpdatePostRequest;
 
 class PostsController extends Controller
@@ -26,7 +27,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('posts.create');
+        return view('posts.create')->with('categories', Category::all());
     }
 
     /**
@@ -46,7 +47,8 @@ class PostsController extends Controller
             'description' => $request->description,
             'content' => $request->content,
             'image' => $image,
-            'published_at' => $request->published_at
+            'published_at' => $request->published_at,
+            'category_id' => $request->category
         ]);
 
         // flash message
@@ -76,7 +78,7 @@ class PostsController extends Controller
      */
     public function edit(Post $post)
     {
-        return view('posts.create')->with('post', $post);
+        return view('posts.create')->with('post', $post)->with('categories', Category::all());
     }
 
     /**
